6a34b2cd2208070d51939a7da32a509cc9541cde
darleen <darleen.e1@schule.bremen.de>
2025-12-18 17:20:55 +0100

    init commit

---
+++ src/main/java/darleinem/kohlkompass/KohlKompassApplication.java
@@ -0,0 +1,13 @@
+package darleinem.kohlkompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KohlKompassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KohlKompassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/darleinem/kohlkompass/KohlKompassApplicationTests.java
@@ -0,0 +1,13 @@
+package darleinem.kohlkompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KohlKompassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
